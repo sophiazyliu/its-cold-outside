2a59ed1a2f5a6ddb053559c70a031847257f6e49
Sophia Liu <sophia_liu@brown.edu>
2021-12-24 15:37:54 -0800

    background video

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import './App.css';
+// import useSound from 'use-sound';
+import {
+  // Row, 
+  // Col, 
+  // Container, 
+  Card, 
+  // CardTitle, 
+  // CardText, 
+  // Button,
+  // CardImg,
+  // CardBody
+} from 'reactstrap';
+import VideoBg from "reactjs-videobg";
+import webm from "./snowing.webm";
+import video from "./snowing.mp4";
+import image from "./snowing.jpeg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+    return (
+      <div className="App">
+        <VideoBg poster={image}>
+          <VideoBg.Source src={webm} type="video/webm" />
+          <VideoBg.Source src={video} type="video/mp4" />
+        </VideoBg>
+  
+        <div className="sounds">
+            <Card />
+        </div>
+      </div>
+    );
+  }
 
 export default App;
